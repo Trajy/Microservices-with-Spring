f75ffaed1f7c9fe364cbaf3f61201bdf789978ae
Trajy <henrique.trajano.a@gmail.com>
2022-08-16 15:55:35 -0300

    hrpayroll project created

---
+++ hrpayroll/src/test/java/trajy/hrpayroll/HrpayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package trajy.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrpayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
