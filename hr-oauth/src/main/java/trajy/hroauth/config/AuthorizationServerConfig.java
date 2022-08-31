ef2186170c40664c167a7d84ce1bf162914bbb3f
Trajy <henrique.trajano.a@gmail.com>
2022-08-31 20:12:09 -0300

    change-hard-coded-secrets-to-properties-from-config-server

--- hr-oauth/src/main/java/trajy/hroauth/config/AuthorizationServerConfig.java
+++ hr-oauth/src/main/java/trajy/hroauth/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package trajy.hroauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,6 +16,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Value("${oauth.client.id}")
+    private String clientId;
+
+    @Value("${oauth.client.secret}")
+    private String clientSecret;
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
@@ -35,8 +42,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient("myappname123")
-            .secret(passwordEncoder.encode("myappsecret123"))
+        clients.inMemory().withClient(clientId)
+            .secret(passwordEncoder.encode(clientSecret))
             .scopes("read", "write").authorizedGrantTypes("password")
             .accessTokenValiditySeconds(86400);
     }
