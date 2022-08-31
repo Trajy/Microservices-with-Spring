d05381b816834430433622dc75a07cd14a4aec5c
Trajy <henrique.trajano.a@gmail.com>
2022-08-31 20:46:58 -0300

    configs-for-cors-policy

--- api-gateway/src/main/java/trajy/apigateway/config/ResourceServerConfig.java
+++ api-gateway/src/main/java/trajy/apigateway/config/ResourceServerConfig.java
@@ -1,12 +1,21 @@
 package trajy.apigateway.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 import static org.springframework.http.HttpMethod.GET;
 
@@ -42,5 +51,31 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
             .antMatchers(PUBLIC_PATHS).permitAll()
             .antMatchers(GET, ROLE_OPERATOR_PATHS).hasAnyRole("OPERATOR", "ADMIN")
             .antMatchers(ROLE_ADMIN_PATHS).hasRole("ADMIN");
+
+        http.cors().configurationSource(getCorsConfigurationSource());
+    }
+
+    @Bean
+    public CorsConfigurationSource getCorsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("*"));
+        corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(Arrays.asList("Authrization", "Content-type"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfig);
+
+        return urlBasedCorsConfigurationSource;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> getCorsFilter() {
+        FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean = new FilterRegistrationBean<>(
+            new CorsFilter(getCorsConfigurationSource())
+        );
+
+        corsFilterFilterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return corsFilterFilterRegistrationBean;
     }
 }
