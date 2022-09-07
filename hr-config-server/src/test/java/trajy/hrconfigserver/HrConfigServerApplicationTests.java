4723ff432fda1f9cf217cfec62382c6883c8e525
Trajy <henrique.trajano.a@gmail.com>
2022-09-07 13:46:56 -0300

    fix-spring-tests
    for-run-mvnw-file-spring-tests-need-the-specification-for-the-tests-class

--- hr-config-server/src/test/java/trajy/hrconfigserver/HrConfigServerApplicationTests.java
+++ hr-config-server/src/test/java/trajy/hrconfigserver/HrConfigServerApplicationTests.java
@@ -2,8 +2,9 @@ package trajy.hrconfigserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import trajy.hrconfigserver.init.HrConfigServerApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = HrConfigServerApplication.class)
 class HrConfigServerApplicationTests {
 
 	@Test
